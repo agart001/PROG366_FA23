2428ff33e97ed4566cb23014b80a51a12cbe2370
agart001 <113141664+agart001@users.noreply.github.com>
2023-11-13 13:15:01 -0600

    Refactor Assignment 1

--- PROG366_Assignment1/PROG366-Assignment1/Program.cs
+++ PROG366_Assignment1/PROG366-Assignment1/Program.cs
@@ -2,11 +2,17 @@
 
 namespace PROG366_Assignment1
 {
+    /// <summary>
+    /// The main program class for demonstrating different time complexities.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// The entry point of the program.
+        /// </summary>
+        /// <param name="args">Command-line arguments (not used in this program).</param>
         static void Main(string[] args)
         {
-
             Print("O(1) Example:", true);
             Print("IsTrue() Checks if a boolean value is true. n = 1, it is called once", true);
             bool O1 = true;
@@ -15,14 +21,14 @@ namespace PROG366_Assignment1
             Print(Environment.NewLine, false);
 
             Print("O(n) Example:", true);
-            Print($"EnumerablePrintAll() prints all the values in an enumerable sequence.{Environment.NewLine}The n is linear, the execution is determined length of the sequence", true);
-            bool[] On = new bool[] { true, false, true, false, true};
+            Print($"EnumerablePrintAll() prints all the values in an enumerable sequence.{Environment.NewLine}The 'n' is linear, and the execution time is determined by the length of the sequence.", true);
+            bool[] On = new bool[] { true, false, true, false, true };
             EnumerablePrintAll(On);
 
             Print(Environment.NewLine, false);
 
             Print("O(n^2) Example:", true);
-            Print($"EnumerablePrintsCounts() prints all the counts of each distinct values in an enumerable sequence.{Environment.NewLine}The n is quadratic, the execution is deteremined by the number of distinct and the length of the sequence", true);
+            Print($"EnumerablePrintCounts() prints all the counts of each distinct value in an enumerable sequence.{Environment.NewLine}The 'n' is quadratic, and the execution time is determined by the number of distinct values and the length of the sequence.", true);
 
             EnumerablePrintCounts(On);
         }
--- PROG366_Assignment1/PROG366-Assignment1/Utility.cs
+++ PROG366_Assignment1/PROG366-Assignment1/Utility.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace PROG366_Assignment1
 {
+    /// <summary>
+    /// A utility class containing various helper methods.
+    /// </summary>
     public static class Utility
     {
+        /// <summary>
+        /// Prints the specified input to the console.
+        /// </summary>
+        /// <param name="input">The input string to be printed.</param>
+        /// <param name="newline">Set to <c>true</c> to print with a newline, or <c>false</c> to print without a newline.</param>
         public static void Print(string input, bool newline)
         {
-            if(newline)
+            if (newline)
             {
                 Console.WriteLine(input);
             }
@@ -20,6 +28,11 @@ namespace PROG366_Assignment1
             }
         }
 
+        /// <summary>
+        /// Checks if the specified boolean value is true.
+        /// </summary>
+        /// <param name="toCheck">The boolean value to check.</param>
+        /// <returns>Returns "Is True" if the value is true, otherwise "Is False."</returns>
         public static string IsTrue(bool toCheck)
         {
             bool check = false;
@@ -30,30 +43,40 @@ namespace PROG366_Assignment1
             return result;
         }
 
+        /// <summary>
+        /// Prints all elements in an enumerable with their indices.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the enumerable.</typeparam>
+        /// <param name="enumerable">The enumerable to print.</param>
         public static void EnumerablePrintAll<T>(IEnumerable<T> enumerable)
         {
             int index = 0;
-            foreach (T value in enumerable) 
+            foreach (T value in enumerable)
             {
                 Print($"{index + 1} : {value}", true);
             }
         }
 
+        /// <summary>
+        /// Prints the counts of distinct elements in an enumerable.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the enumerable.</typeparam>
+        /// <param name="enumerable">The enumerable to analyze and print counts for distinct elements.</param>
         public static void EnumerablePrintCounts<T>(IEnumerable<T> enumerable)
         {
             IEnumerable<T> distinct = enumerable.Distinct();
 
-
-            foreach(T distinctvalue in distinct)
+            foreach (T distinctvalue in distinct)
             {
                 int count = 0;
-                foreach(T value in enumerable)
+                foreach (T value in enumerable)
                 {
-                    if(value.Equals(distinctvalue)) count += 1;
+                    if (value.Equals(distinctvalue)) count += 1;
                 }
 
                 Print($"{distinctvalue} : {count}", true);
             }
         }
     }
+
 }
