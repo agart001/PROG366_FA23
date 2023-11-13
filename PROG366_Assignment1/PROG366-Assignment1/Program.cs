fb75a41c9fbdfc919f228f3bd68a3f2408ddff0b
agart001 <113141664+agart001@users.noreply.github.com>
2023-11-13 13:07:04 -0600

    Added Assignment 1

---
+++ PROG366_Assignment1/PROG366-Assignment1/Program.cs
@@ -0,0 +1,30 @@
+﻿using static PROG366_Assignment1.Utility;
+
+namespace PROG366_Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Print("O(1) Example:", true);
+            Print("IsTrue() Checks if a boolean value is true. n = 1, it is called once", true);
+            bool O1 = true;
+            Print(IsTrue(O1), true);
+
+            Print(Environment.NewLine, false);
+
+            Print("O(n) Example:", true);
+            Print($"EnumerablePrintAll() prints all the values in an enumerable sequence.{Environment.NewLine}The n is linear, the execution is determined length of the sequence", true);
+            bool[] On = new bool[] { true, false, true, false, true};
+            EnumerablePrintAll(On);
+
+            Print(Environment.NewLine, false);
+
+            Print("O(n^2) Example:", true);
+            Print($"EnumerablePrintsCounts() prints all the counts of each distinct values in an enumerable sequence.{Environment.NewLine}The n is quadratic, the execution is deteremined by the number of distinct and the length of the sequence", true);
+
+            EnumerablePrintCounts(On);
+        }
+    }
+}
---
+++ PROG366_Assignment1/PROG366-Assignment1/Utility.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG366_Assignment1
+{
+    public static class Utility
+    {
+        public static void Print(string input, bool newline)
+        {
+            if(newline)
+            {
+                Console.WriteLine(input);
+            }
+            else
+            {
+                Console.Write(input);
+            }
+        }
+
+        public static string IsTrue(bool toCheck)
+        {
+            bool check = false;
+            string result = "Is False";
+
+            if (toCheck != check) result = "Is True";
+
+            return result;
+        }
+
+        public static void EnumerablePrintAll<T>(IEnumerable<T> enumerable)
+        {
+            int index = 0;
+            foreach (T value in enumerable) 
+            {
+                Print($"{index + 1} : {value}", true);
+            }
+        }
+
+        public static void EnumerablePrintCounts<T>(IEnumerable<T> enumerable)
+        {
+            IEnumerable<T> distinct = enumerable.Distinct();
+
+
+            foreach(T distinctvalue in distinct)
+            {
+                int count = 0;
+                foreach(T value in enumerable)
+                {
+                    if(value.Equals(distinctvalue)) count += 1;
+                }
+
+                Print($"{distinctvalue} : {count}", true);
+            }
+        }
+    }
+}
